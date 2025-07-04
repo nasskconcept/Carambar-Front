609bf3f784f319290af6eb5d14a10e9e0eb6f2a0
nasskconcept <mehdinassima59@icloud.com>
2025-07-04 13:46:19 +0200

    feat(api): connect front to deployed API on Render

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const API_URL =
-  "https://carambar-back-1-490f.onrender.com/api/v1/blagues/random";
+  "https://carambar-back-1-496f.onrender.com/api/v1/blagues/random";
 
 async function getRandomBlague() {
   const questionBox = document.getElementById("question");
